c3ddf71cb20eb4b5c6b82094b2d5900121139a05
GorudoYami <gorudoyami@gmail.com>
2024-05-08 20:05:29 +0200

    Read data with SensorsListener

--- app/src/main/java/pl/polsl/sensordatacollector/sensors/SensorsListener.kt
+++ app/src/main/java/pl/polsl/sensordatacollector/sensors/SensorsListener.kt
@@ -3,17 +3,36 @@ package pl.polsl.sensordatacollector.sensors
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
 
-class SensorsListener : SensorEventListener {
+class SensorsListener(private val sensorManager: SensorManager) : SensorEventListener {
+    private val _sensorDelay = SensorManager.SENSOR_DELAY_NORMAL
+    private val _sensorTypes = listOf(Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_MAGNETIC_FIELD, Sensor.TYPE_LIGHT)
     private val _data: MutableMap<Int, MutableList<FloatArray>> = mutableMapOf()
+    fun start() {
+        for (sensorType in _sensorTypes) {
+            val sensor = sensorManager.getDefaultSensor(sensorType)
+            sensorManager.registerListener(this, sensor, _sensorDelay)
+        }
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+    }
 
     fun getValues(sensorType: Int): List<FloatArray>? {
         return _data[sensorType]
     }
 
+    fun clearValues() {
+        _data.clear()
+    }
+
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null) {
             appendValues(event.values, event.sensor.type)
+            Log.d("SensorData",  "${event.sensor.type}: ${event.values.joinToString(separator = ", ")}")
         }
     }
 
